f81706c90797a7610071380261e37488f9d06e6b
khalil <khalillakhdharatc@gmail.com>
2023-05-11 11:07:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,5 +5,9 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 4000;
 // connexion à la base de données
-mongoose.connect('mongodb://localhost:27017/name',
- { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://khalillakhdhar:Jahealx3IoYtRVOT@cluster0.lhgfryr.mongodb.net/?retryWrites=true&w=majority',
+ { useNewUrlParser: true, useUnifiedTopology: true })
+ .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch((err) => console.log('Connexion à MongoDB échouée !'+ err));
+ 
+ 
